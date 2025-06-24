5695f06a193bffc47a53c812b1a1f5ef1e52fb01
c-n8 <bold.spot3954@alianmail.com>
2025-06-24 16:25:45 -0700

    feat(agentdock-core): add Jest testing infrastructure
    Add missing Jest dependencies and configuration to enable execution of 34 existing test files in agentdock-core package.

---
+++ agentdock-core/jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
+  testMatch: ['**/__tests__/**/*.test.ts', '**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts', '!src/test/**/*']
+};
